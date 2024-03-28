23facaf14e75f8023cdc12ccd9fb30125d79f779
Thais Simoes <thay.ssim@gmail.com>
2024-03-27 22:22:37 -0300

    retirada de wait group para melhora de performance

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
-	"sync"
 	"time"
 )
 
@@ -29,9 +28,6 @@ func rotas(s *gin.Engine) {
 }
 
 func getCEP(c *gin.Context) {
-	wg := sync.WaitGroup{}
-	wg.Add(3)
-
 	channel := make(chan string)
 
 	var openCEPAPI openCEPAPI
@@ -46,11 +42,11 @@ func getCEP(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(c, _requestTimeout)
 	defer cancel()
 
-	brasiAPIURL := _brasilAPIURL + cep
+	brasilAPIURL := _brasilAPIURL + cep
 	openCEPAPIURL := _openCepAPIURL + cep
 	viaCepAPIURL := _viaCepAPIURL + cep + "/json"
 
-	reqBrasilAPI, err := http.NewRequestWithContext(ctx, http.MethodGet, brasiAPIURL, nil)
+	reqBrasilAPI, err := http.NewRequestWithContext(ctx, http.MethodGet, brasilAPIURL, nil)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"err": err,
@@ -74,83 +70,90 @@ func getCEP(c *gin.Context) {
 	go func() {
 		start := time.Now()
 		respBrasilAPI, err = http.DefaultClient.Do(reqBrasilAPI)
-		if err != nil {
+		if err != nil || respBrasilAPI.StatusCode != http.StatusOK {
 			select {
 			case <-ctx.Done():
 				log.Print("Brasil API - tempo de contexto excedido")
-				channel <- "Error"
-				wg.Done()
+				return
 			default:
 				log.Printf("Brasil API - erro na solicitação. err -> %v", err)
-				channel <- "Error"
-				wg.Done()
+				return
 			}
 		}
-		wg.Done()
-		channel <- "Brasil API"
 		elapsed := time.Since(start)
 		log.Printf("Brasil API - %s", elapsed)
+		channel <- "Brasil API"
 	}()
 
 	go func() {
 		start := time.Now()
 		respOpenCEPAPI, err = http.DefaultClient.Do(reqOpenCEPAPI)
-		if err != nil {
+		if err != nil || respOpenCEPAPI.StatusCode != http.StatusOK {
 			select {
 			case <-ctx.Done():
 				log.Print("Open CEP API - tempo de contexto excedido")
-				wg.Done()
-				channel <- "Error"
+				return
 			default:
 				log.Printf("Open CEP API - erro na solicitação. err -> %v", err)
-				wg.Done()
-				channel <- "Error"
+				return
 			}
 		}
-		wg.Done()
-		channel <- "Open CEP API"
 		elapsed := time.Since(start)
 		log.Printf("Open CEP API - %s", elapsed)
+		channel <- "Open CEP API"
+
 	}()
 
 	go func() {
 		start := time.Now()
 		respViaCepAPI, err = http.DefaultClient.Do(reqViaCepAPI)
-		if err != nil {
+		if err != nil || respViaCepAPI.StatusCode != http.StatusOK {
 			select {
 			case <-ctx.Done():
 				log.Print("Via CEP - tempo de contexto excedido")
-				wg.Done()
-				channel <- "Error"
+				return
 			default:
 				log.Printf("Via CEP API - erro na solicitação. err -> %v", err)
-				wg.Done()
-				channel <- "Error"
+				return
 			}
 		}
-		wg.Done()
-		channel <- "Via CEP API"
 		elapsed := time.Since(start)
 		log.Printf("Via CEP - %s", elapsed)
+		channel <- "Via CEP API"
+
 	}()
 
-	wg.Wait()
 	API := <-channel
 
 	switch API {
 	case "Via CEP API":
 		json.NewDecoder(respViaCepAPI.Body).Decode(&viaCEPAPI)
 		viaCEPAPI.API = "Via CEP API"
+		log.Printf("api resposta: %s. informações de retorno: %v", viaCEPAPI.API, viaCEPAPI)
 		c.JSON(http.StatusOK, viaCEPAPI)
 	case "Brasil API":
 		json.NewDecoder(respBrasilAPI.Body).Decode(&brasilAPI)
 		brasilAPI.API = "Brasil API"
+		log.Printf("api resposta: %s. informações de retorno: %v", brasilAPI.API, brasilAPI)
 		c.JSON(http.StatusOK, brasilAPI)
 	case "Open CEP API":
 		json.NewDecoder(respOpenCEPAPI.Body).Decode(&openCEPAPI)
 		openCEPAPI.API = "Open CEP API"
+		log.Printf("api resposta: %s. informações de retorno: %v", openCEPAPI.API, openCEPAPI)
 		c.JSON(http.StatusOK, openCEPAPI)
 	default:
-		c.JSON(http.StatusInternalServerError, "erro ao realizar a chamada API")
+		select {
+		case <-ctx.Done():
+			log.Printf("Contexto excedido")
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "contexto excedido",
+			})
+		default:
+			c.JSON(http.StatusBadRequest, gin.H{
+				"message": "erro na chamada api",
+				"err":     err,
+			})
+		}
 	}
+
 }
